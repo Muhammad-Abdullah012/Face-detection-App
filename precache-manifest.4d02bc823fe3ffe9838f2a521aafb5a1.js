ab5b782e41e91fe4c9f772b930518c9b9ac75275
Abdullah <ma9589094@gmail.com>
2021-04-24 13:46:15 +0500

    Updates

---
+++ precache-manifest.4d02bc823fe3ffe9838f2a521aafb5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4d052995bed477a98843066eaf96a2",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "e60f73dd23233e39a80d",
+    "url": "/facedetection/static/css/main.bc37d4e3.chunk.css"
+  },
+  {
+    "revision": "a9e671a49149bac78910",
+    "url": "/facedetection/static/js/2.3284d0dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/facedetection/static/js/2.3284d0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60f73dd23233e39a80d",
+    "url": "/facedetection/static/js/main.253f5d2c.chunk.js"
+  },
+  {
+    "revision": "99d29fa1a4ca556ebf26",
+    "url": "/facedetection/static/js/runtime-main.7cf075ce.js"
+  }
+]);
